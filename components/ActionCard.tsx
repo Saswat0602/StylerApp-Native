f179e00e9ca76033661bbcbe7ce6ed89efa51c5e
saswat <saswatranjan0602@gmail.com>
2023-09-19 19:06:58 +0530

    adding styles to action card

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -1,65 +1,119 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable prettier/prettier */
 
-import { StyleSheet, Text, View, Linking, Image,TouchableOpacity } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Linking,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
 
 export default function ActionCard() {
-  function openWebsite(websiteLink:string){
-    Linking.openURL(websiteLink)
+  function openWebsite(websiteLink: string) {
+    Linking.openURL(websiteLink);
   }
+
   return (
     <View>
-      <Text style={styles.heading}>Blog Card</Text>
-      <View style={[styles.heading, styles.elevatedCard]}>
-        <View style={styles.headingCont}>
-          <Text style={styles.headerText}>Whats new in React 18.0</Text>
+      <Text style={styles.headingText}>Blog Card</Text>
+      <View style={[styles.card, styles.elevatedCard]}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headerText}>
+            What's new in Javascript 21 - ES12
+          </Text>
         </View>
         <Image
           source={{
-            uri: 'https://unsplash.com/photos/a-hand-holding-a-white-paper-cut-out-of-a-video-play-button-KPRNbxRrl44',
+            uri: 'https://images.unsplash.com/photo-1683009427692-8a28348b0965?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
           }}
           style={styles.cardImage}
         />
-        <View style={styles.bodyConatainer}>
-          <Text numberOfLines={2}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ut
-            cupiditate repudiandae placeat quis accusamus consequatur at
-            officiis facere delectus? Quas sit modi assumenda nemo quaerat fuga
-            repellat distinctio incidunt.
+        <View style={styles.bodyContainer}>
+          <Text style={styles.black} numberOfLines={3}>
+            Just like every year, Javascript brings in new features. This year
+            javascript is bringing 4 new features, which are almost in
+            production rollout. I won't be wasting much more time and directly
+            jump to code with easy to understand examples.
           </Text>
         </View>
-
-        <View style={styles.footerConatainer}>
+        <View style={styles.footerContainer}>
           <TouchableOpacity
             onPress={() => openWebsite('https://github.com/Saswat0602')}>
-            <Text style={styles.socials}>Find More...</Text>
+            <Text style={styles.socialLinks}>Read More</Text>
           </TouchableOpacity>
-
           <TouchableOpacity
             onPress={() => openWebsite(' https://saswatranjan.netlify.app/')}>
-            <Text style={styles.socials}>Visit Here...</Text>
+            <Text style={styles.socialLinks}>Follow me</Text>
           </TouchableOpacity>
         </View>
       </View>
     </View>
   );
 }
- 
 
 const styles = StyleSheet.create({
-  heading: {
-    fontSize: 20,
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 16,
+    marginTop: 20,
   },
-  elevatedCard: {},
-  headingCont: {},
-  headerText: {},
+  card: {
+    width: 350,
+    borderRadius: 10,
+    marginVertical: 16,
+    marginHorizontal: 16,
+    backgroundColor: '#fff',
+  },
+  elevatedCard: {
+    elevation: 5,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowColor: '#000',
+    shadowOpacity: 0.3,
+  },
+  headingContainer: {
+    height: 40,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'teal',
+ 
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
   cardImage: {
-    height: 376,
+    height: 200,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
   },
-  bodyConatainer: {},
-  footerConatainer: {},
-  socials:{
-
+  bodyContainer: {
+    padding: 16,
+    color: 'black',
+  },
+  footerContainer: {
+    padding: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  socialLinks: {
+    fontSize: 16,
+    color: '#fff',
+    backgroundColor: 'teal',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 6,
+    textAlign: 'center',
+  },
+  black:{
+    color:'#000',
   },
 });
